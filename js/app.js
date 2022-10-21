81bbd1aa5163869eca3664be9a3ea424e01e3688
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-20 23:25:06 -0500

    Modificando ruta de SW

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 
+var url = window.location.href;
+var swLocation = '/Dispositivo-Twittor/sw.js';
+
 
 if( navigator.serviceWorker ) {
-    navigator.serviceWorker.register('/sw.js');
+
+
+    if( url.includes('localhost') ){
+        swLocation = '/sw.js';
+    }
+
+
+    navigator.serviceWorker.register(swLocation);
 }
 
 
