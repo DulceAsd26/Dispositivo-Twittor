25a6198915d23313410f14234dd76010868731ba
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-20 21:58:38 -0500

    Cache con Network Fallback

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
+//imports
+importScripts('js/sw-utils.js');
 
 
-
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -16,7 +17,8 @@ const APP_SHELL = [
     'img/avatars/spiderman.jpg',
     'img/avatars/thor.jpg',
     'img/avatars/wolverine.jpg',
-    'js/app.js'
+    'js/app.js',
+    'js/sw-utils.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -60,6 +62,31 @@ self.addEventListener('activate', e => {
     });
 
     e.waitUntil( respuesta);
+
+});
+
+
+
+self.addEventListener('fetch', e => {
+
+
+    const respuesta = caches.match( e.request ).then( res => {
+
+        if( res ){
+            return res;
+        }else{
+
+            return fetch(e.request).then( newRes =>{
+                
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+            });
+
+        }
+
+
+
+    });
+
 
 
 });
