591f187444dfff9a5c7961cd31cc7c29b8491fee
DulceAsd26 <juarez.dulce.1jm@gmail.com>
2022-10-21 00:04:19 -0500

    Realizando cambios

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('js/sw-utils.js');
 
 
 const STATIC_CACHE = 'static-v2';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -55,6 +55,11 @@ self.addEventListener('activate', e => {
 
             if( key != STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
+            }
+
+            if( key != STATIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+
             }
 
         });
